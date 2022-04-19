66f8f6e22194706314bb690f778b2e6c872ada8e
A Erdem Ergin <a.erdemergin@gmail.com>
2022-04-20 02:39:53 +0300

    Teacher class

--- src/notHesap/Teacher.java
+++ src/notHesap/Teacher.java
@@ -6,6 +6,46 @@ public class Teacher {
     private String name;
     private String branch;
 
+    public Teacher() {
 
+    }
 
+    public Teacher(String teacherId, String name, String branch) {
+        this.teacherId = teacherId;
+        this.name = name;
+        this.branch = branch;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "teacherId='" + teacherId + '\'' +
+                ", name='" + name + '\'' +
+                ", branch='" + branch + '\'' +
+                '}';
+    }
 }
